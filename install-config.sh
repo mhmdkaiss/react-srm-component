352422a66cf3e209a3a2563ec8bb03fa3aaa759e
Harshil Sethi <harshil.sethi@nicecactus.gg>
2021-11-16 14:10:17 +0000

    NPTF-239 - Add lint error with option -p

--- install-config.sh
+++ install-config.sh
@@ -1,4 +1,18 @@
-cp ./node_modules/@cactus/srm-component/dist/config/.eslintrc ./
+#!/bin/bash
 cp ./node_modules/@cactus/srm-component/dist/config/.eslintignore ./
 cp ./node_modules/@cactus/srm-component/dist/config/.prettierrc ./
 cp ./node_modules/@cactus/srm-component/dist/config/.stylelintrc ./
+
+ERROR='false'
+while getopts "p" option; do
+  case ${option} in
+    p ) #ERROR
+      ERROR='true'
+    ;;
+  esac
+done
+if ${ERROR}; then
+    cp ./node_modules/@cactus/srm-component/dist/config/.eslintrc-error ./.eslintrc
+else
+    cp ./node_modules/@cactus/srm-component/dist/config/.eslintrc ./.eslintrc
+fi
