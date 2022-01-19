3897f1c89bfdd9970aff38f902e39e90a5ed41e5
Dylan SAVARESSE <dylan.savaresse@nicecactus.gg>
2022-01-19 13:44:47 +0000

    Merged in bugfix/NPTF-434-link-are-not-clickable-in-chatb (pull request #137)
    NPTF-434 Clickable links in chatbox
    
    * NPTF-434 Clickable links in chatbox
    
    * Fix for multi links in message
    
    * white links
    
    * who put those logs???
    
    * indentation fix
    
    
    Approved-by: Harshil Sethi
    Approved-by: Bruno HRELJA

--- src/components/Chat/Messages.tsx
+++ src/components/Chat/Messages.tsx
@@ -11,14 +11,21 @@ export interface ChatProps {
 
 export const Messages: React.FunctionComponent<ChatProps> = ({ messages, currentUserId, fullScreen }) => {
     const intl = useIntl();
-
+    // eslint-disable-next-line no-useless-escape
+    const isUrlRegex = /(((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$))/gi;
     return (
         <div className={`px-3 mt-2 messages-component messages d-flex flex-column flex-column-reverse ${fullScreen ? 'fullscreen-chat' : ''}`}>
             {messages.map((m) => {
                 const isReferee = m.senderName === 'ADMIN';
                 const isMe = currentUserId && m.sender === currentUserId;
+                let message = m.content.message;
                 let name: string;
                 let code: string | undefined;
+
+                const links = message.match(isUrlRegex);
+                links?.forEach((element:string) => {
+                    message = message.replaceAll(element, `<a style="color:white" href='${element}' target="_blank" rel="noopener noreferrer">${element}<a>`);
+                });
 
                 if (isReferee) {
                     name = intl.formatMessage({ id: 'chat.referee' });
@@ -33,18 +40,18 @@ export const Messages: React.FunctionComponent<ChatProps> = ({ messages, current
                     <div>
                         <span className="username">{name}</span>
                         {code &&
-                            <span className="code">
-                                {code}
-                                {isMe && <span className="ml-1">(
-                                    <FormattedMessage
-                                        id="chat.me"
-                                        description="Chat - Me"
-                                    />
-                                    )</span>}
-                            </span>
+                        <span className="code">
+                            {code}
+                            {isMe && <span className="ml-1">(
+                                <FormattedMessage
+                                    id="chat.me"
+                                    description="Chat - Me"
+                                />
+                                )</span>}
+                        </span>
                         }
                     </div>
-                    <div className="message mt-2">{m.content.message}</div>
+                    <div dangerouslySetInnerHTML={{ __html: message }} className='message mt-2'></div>
                 </div>;
             })}
         </div>
