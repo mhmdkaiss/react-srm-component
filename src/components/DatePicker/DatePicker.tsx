956201623c22d33f198fdbee4303a379aed16a01
Fabien Bruzzi <fabien.bruzzi@nicecactus.gg>
2022-02-09 08:52:09 +0000

    Merged in NPTF-499-bo-dashboard-kpi-1 (pull request #160)
    NPTF-499 - Update components for bo dashboard
    
    * NPTF-499 - Update components for bo dashboard
    
    
    Approved-by: Harshil Sethi
    Approved-by: Etienne Esclapez

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -8,25 +8,39 @@ import { ThemePlatform } from '../../styles/Themes';
 export interface DatePickerProps {
     label: string;
     initialDate?: string | number;
+    value?: string | number;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     dateChanged: (date?: string) => any;
 }
 
 export const DatePicker: React.FunctionComponent<DatePickerProps> = (props: DatePickerProps) => {
+    const val = props.value ?
+        new Date(props.value || '').getTime() ? new Date(props.value).toISOString().slice(0, 16) : props.value : undefined;
     const defaultVal = props.initialDate ?
         new Date(props.initialDate || '').getTime() ? new Date(props.initialDate).toISOString().slice(0, 16) : props.initialDate : undefined;
     return (
         <React.Fragment>
             <MuiThemeProvider theme={ThemePlatform}>
                 <div className="datepicker-container d-flex w-100 position-relative">
-                    <TextField
-                        type="datetime-local"
-                        className="w-100 datepicker-input nicecactus-input"
-                        defaultValue={defaultVal}
-                        label={props.label}
-                        onChange={(event: ChangeEvent<HTMLInputElement>) => { props.dateChanged(event.target.value); }}
-                        InputLabelProps={{ shrink: true }}
-                    />
+                    {
+                        val ?
+                            <TextField
+                                type="datetime-local"
+                                className="w-100 datepicker-input nicecactus-input"
+                                value={val}
+                                label={props.label}
+                                onChange={(event: ChangeEvent<HTMLInputElement>) => { props.dateChanged(event.target.value); }}
+                                InputLabelProps={{ shrink: true }}
+                            /> :
+                            <TextField
+                                type="datetime-local"
+                                className="w-100 datepicker-input nicecactus-input"
+                                defaultValue={defaultVal}
+                                label={props.label}
+                                onChange={(event: ChangeEvent<HTMLInputElement>) => { props.dateChanged(event.target.value); }}
+                                InputLabelProps={{ shrink: true }}
+                            />
+                    }
                     <div
                         className="icon"
                         style={{
--- src/components/GameList/GameList.tsx
+++ src/components/GameList/GameList.tsx
@@ -1,6 +1,6 @@
 import './GameList.scss';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { ButtonIcon } from '../../atoms/Button/ButtonIcon';
 import { ButtonSize } from '../../atoms/Button/Button';
@@ -9,16 +9,39 @@ import { Game } from '../../models/Game';
 export interface GameListProps {
     games: Array<Game>;
     fancy?: boolean,
-    onChange: (game: Game) => void;
+    multiple?: boolean,
+    defaultSelected?: Array<number>,
+    onChange?: (game: Game) => void;
+    selectedChanged?: (selected: Array<number>) => void;
 }
 
 export const GameList: React.FunctionComponent<GameListProps> = (props: GameListProps) => {
-    const [ selected, setSelected ] = useState<number>(0);
+    const [ selected, setSelected ] = useState<Array<number>>(props.defaultSelected || [0]);
+
+    useEffect(() => {
+        if (props.selectedChanged) {
+            props.selectedChanged(selected);
+        }
+    }, [selected]);
 
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         const newValue = parseInt(event.currentTarget.value);
-        setSelected(newValue);
-        props.onChange(props.games[newValue]);
+
+        if (props.multiple) {
+            const index = selected.findIndex(i => i === newValue);
+            if (index > -1) {
+                selected.splice(index, 1);
+                setSelected(selected);
+            } else {
+                setSelected([ ...selected, newValue ]);
+            }
+        } else {
+            setSelected([newValue]);
+        }
+
+        if (props.onChange) {
+            props.onChange(props.games[newValue]);
+        }
     };
 
     return (
@@ -30,7 +53,7 @@ export const GameList: React.FunctionComponent<GameListProps> = (props: GameList
                         size={ButtonSize.BIG}
                         icon={game.icon}
                         name={game.id}
-                        active={selected === index}
+                        active={selected.includes(index)}
                         onClick={handleClick}
                         key={index}
                         fancy={props.fancy}
