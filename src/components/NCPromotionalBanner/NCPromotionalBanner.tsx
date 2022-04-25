7bc11810e61ca804668a06fdaca9a1b0a77606ac
Gaëtan POLLIART <gaetan.polliart@nicecactus.gg>
2022-04-25 07:14:46 +0000

    Merged in OSS-39-promotion-banner-without-text (pull request #224)
    OSS-39 : update text check for promotional banner
    
    * update text check for promotional banner
    
    * better way to have full banner if no text
    
    
    Approved-by: Alessandro Dessi
    Approved-by: mohamad kaiss
    Approved-by: Philippe Lefevre

--- src/components/NCPromotionalBanner/NCPromotionalBanner.tsx
+++ src/components/NCPromotionalBanner/NCPromotionalBanner.tsx
@@ -22,7 +22,7 @@ export const NCPromotionalBanner: React.FunctionComponent<NCPromotionalBannerPro
 
     return (
         <div
-            className={`nc-promo-banner ${props.buttonLink && !props.buttonText ? 'clickable' : ''} ${!props.buttonText && !props.text ? 'full' : ''}`}
+            className={`nc-promo-banner ${props.buttonLink && !props.buttonText ? 'clickable' : ''} ${!props.buttonText?.length && !props.text?.length && 'full'}`}
             onClick={() => {
                 if (props.buttonLink && !props.buttonText) {
                     openLink();
